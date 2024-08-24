40e1df290a21fd649500b7a66cc60534673a4aec
Guillermo <guillewu@gmail.com>
2024-08-24 18:22:17 +0200

    Add 3 categories

--- leaderboard.js
+++ leaderboard.js
@@ -1,4 +1,4 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from "meteor/meteor";
 import { Template } from "meteor/templating";
 import { Mongo } from "meteor/mongo";
 import { Session } from "meteor/session";
@@ -9,48 +9,84 @@ import { Random } from "meteor/random";
 
 const Players = new Mongo.Collection("players");
 
+const POINTS_BY_CATEGORY = {
+  Scientist: 10,
+  Athlete: 5,
+  Actor: 15,
+};
+
 if (Meteor.isClient) {
-  Template.leaderboard.helpers({
-    players: function () {
-      return Players.find({}, { sort: { score: -1, name: 1 } });
-    },
-    selectedName: function () {
+  Template.main.helpers({
+    selectedPlayer: function () {
       const player = Players.findOne(Session.get("selectedPlayer"));
-      return player && player.name;
-    }
+      return { ...player, pointsToAdd: POINTS_BY_CATEGORY[player.category] };
+    },
   });
 
-  Template.leaderboard.events({
-    'click .inc': function () {
-      Players.update(Session.get("selectedPlayer"), {$inc: {score: 5}});
-    }
+  Template.main.events({
+    "click .inc": function () {
+      const player = Players.findOne(Session.get("selectedPlayer"));
+      const category = player.category;
+      Players.update(player._id, {
+        $inc: { score: POINTS_BY_CATEGORY[category] },
+      });
+    },
+  });
+
+  Template.leaderboard.helpers({
+    players: function () {
+      return Players.find(
+        { category: this.category },
+        { sort: { score: -1, name: 1 } }
+      );
+    },
   });
 
   Template.player.helpers({
     selected: function () {
-      return Session.equals("selectedPlayer", this._id) ? "selected" : '';
-    }
+      return Session.equals("selectedPlayer", this._id) ? "selected" : "";
+    },
   });
 
   Template.player.events({
-    'click': function () {
+    click: function () {
       Session.set("selectedPlayer", this._id);
-    }
+    },
   });
 }
 
 // On server startup, create some players if the database is empty.
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    if (Players.find().count() === 0) {
-      const names = ["Ada Lovelace", "Grace Hopper", "Marie Curie",
-                   "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
-      names.forEach(function (name) {
-        Players.insert({
-          name: name,
-          score: Math.floor(Random.fraction() * 10) * 5
+  Meteor.startup(async () => {
+    const users = [
+      { name: "Sc. Ada Lovelace", category: "Scientist" },
+      { name: "Sc. Grace Hopper", category: "Scientist" },
+      { name: "Sc. Marie Curie", category: "Scientist" },
+      { name: "Sc. Carl Friedrich Gauss", category: "Scientist" },
+      { name: "Sc. Nikola Tesla", category: "Scientist" },
+      { name: "Sc. Claude Shannon", category: "Scientist" },
+      { name: "Ath. Ada Lovelace", category: "Athlete" },
+      { name: "Ath. Grace Hopper", category: "Athlete" },
+      { name: "Ath. Marie Curie", category: "Athlete" },
+      { name: "Ath. Carl Friedrich Gauss", category: "Athlete" },
+      { name: "Ath. Nikola Tesla", category: "Athlete" },
+      { name: "Ath. Claude Shannon", category: "Athlete" },
+      { name: "Act. Ada Lovelace", category: "Actor" },
+      { name: "Act. Grace Hopper", category: "Actor" },
+      { name: "Act. Marie Curie", category: "Actor" },
+      { name: "Act. Carl Friedrich Gauss", category: "Actor" },
+      { name: "Act. Nikola Tesla", category: "Actor" },
+      { name: "Act. Claude Shannon", category: "Actor" },
+    ];
+
+    const playerCount = await Players.find().countAsync();
+    if (playerCount === 0) {
+      for (const user of users) {
+        await Players.insertAsync({
+          ...user,
+          score: Math.floor(Random.fraction() * 10) * 5,
         });
-      });
+      }
     }
   });
 }
